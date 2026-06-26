4243607cb1f3afb72b48c5500aaa907aed85296b
Leonid Livshits <141512533+leonidlivshits@users.noreply.github.com>
2026-06-26 19:07:48 +0300

    Update 001_create_price_history.sql

--- migrations/001_create_price_history.sql
+++ migrations/001_create_price_history.sql
@@ -1,6 +1,6 @@
 create table if not exists price_history (
     id bigserial primary key,
-    good_id bigint not null,
-    create_at timestamptz not null default clock_timestamp(),
+    good_id bigint not null check (good_id > 0),
+    create_at timestamptz not null default now(),
     price integer not null check (price >= 0)
 );
